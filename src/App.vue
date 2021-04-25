8861aa1abaf4ed1ceb8a9dd15bd44133fb3e934b
daluisgarcia <daluisgarcia@gmail.com>
2021-04-24 22:08:10 -0400

    Introduction to components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <User v-for="user in users"
+          v-bind:key="user.id"
+          :name="user.name"
+          :email="user.email"
+          :phone="user.phone"
+          :address="user.address"></User>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import User from "./components/User";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    User
+  },
+  data(){
+    return {
+      users: []
+    }
+  },
+  methods:{
+    getUsers(){
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then( (response) => this.users = response.data )
+    }
+  },
+  mounted() {
+    this.getUsers();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Name: {{name}}</p>
+        <p>Email: {{email}}</p>
+        <p>Phone: {{phone}}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        //props: ['name', 'email','phone'] Regular props
+        props: { //Props with validations
+            name:String,
+            email:String,
+            phone:String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
